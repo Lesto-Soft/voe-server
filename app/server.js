f95b0f23b61e5f71763e89624ff982521f7f9994
bobi1995 <borislav.stefanov.1995@gmail.com>
2025-03-10 16:43:25 +0200

    Setup the server.js with initial architecture

--- app/server.js
+++ app/server.js
@@ -1,9 +1,21 @@
 import cors from "cors";
 import express from "express";
+import dotenv from "dotenv";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+import { readFile } from "node:fs/promises";
+import { resolvers } from "./graphql/resolvers.js";
+dotenv.config();
+const typeDefs = await readFile("app/graphql/schema.graphql", "utf8");
 
 const app = express();
 app.use(cors(), express.json());
+app.use("/static", express.static("public"));
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+app.use("/graphql", apolloMiddleware(apolloServer));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
